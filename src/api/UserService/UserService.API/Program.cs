3c09a48b865f1a6e46b5edb0fb7a79b9de909203
Hein Taljaard <hein.taljaard@gmail.com>
2025-10-10 09:05:53 +1100

    Add Identity, scaffolding, and FakeEmailSender service
    - Added `Microsoft.VisualStudio.Web.CodeGeneration.Design` package for scaffolding in `Directory.Packages.props` and `UserService.API.csproj`.
    - Updated `Program.cs` to register `AddAuthorization`, `AddServiceDefaults`, and Identity services.
    - Registered `FakeEmailSender` as a singleton for `IEmailSender<ApplicationUser>`.
    - Added `FakeEmailSender` class with unimplemented methods for email operations.
    - Added `.csproj.user` files for `PatientService.API` and `UserService.API` to configure debugging profiles and scaffolding settings.
    - Performed minor code cleanup in `Program.cs`.

--- src/api/UserService/UserService.API/Program.cs
+++ src/api/UserService/UserService.API/Program.cs
@@ -5,6 +5,7 @@ using Scalar.AspNetCore;
 
 using UserService.API.Data;
 using UserService.API.Domain;
+using UserService.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,10 +14,15 @@ builder.AddSqlServerDbContext<AppDbContext>("PatientApp");
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddAuthorization();
+
+
 builder.AddServiceDefaults(); //Aspire
 
 // builder.Services.AddDbContext<AppDbContext>(o =>
 //                     o.UseSqlServer(builder.Configuration.GetConnectionString("UserServiceDb")));
+
+builder.Services.AddSingleton<IEmailSender<ApplicationUser>,FakeEmailSender>();
 
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>();
 
---
+++ src/api/UserService/UserService.API/Services/FakeEmailSender.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+
+using UserService.API.Domain;
+
+namespace UserService.API.Services;
+
+public class FakeEmailSender : IEmailSender<ApplicationUser>
+{
+    public Task SendConfirmationLinkAsync(ApplicationUser user, string email, string confirmationLink)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendPasswordResetCodeAsync(ApplicationUser user, string email, string resetCode)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SendPasswordResetLinkAsync(ApplicationUser user, string email, string resetLink)
+    {
+        throw new NotImplementedException();
+    }
+}
